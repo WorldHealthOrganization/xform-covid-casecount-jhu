1c3026ff18b6ae6aaba52cfe8fd80c1f25cfae07
Thijs Cornelissen <cornelissent@who.int>
2023-01-18 14:35:09 +0100

    Update xform.R

--- src/xform.R
+++ src/xform.R
@@ -27,7 +27,7 @@ process <- function(x, name) {
 
   x %>%
     tidyr::pivot_longer(
-      cols = ends_with("20") | ends_with("21") | ends_with("22"),
+      cols = ends_with("20") | ends_with("21") | ends_with("22") | ends_with("23"),
       names_to = "date",
       values_to = name) %>%
     dplyr::rename_all(rnm) %>%
